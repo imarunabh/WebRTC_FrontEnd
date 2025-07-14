d6488abf13618f11973884b6e40e721187d4d739
Arunabh <arunabhsharma54321@gmail.com>
2025-07-14 22:05:12 +0545

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './services/test-service.service';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <h1>Angular + Spring Boot Test</h1>
+    <button (click)="testConnection()">Test Connection</button>
+    <p *ngIf="response">{{ response }}</p>
+  `
 })
-export class AppComponent {
-  title = 'webRTC';
+export class AppComponent implements OnInit {
+  response: string | undefined;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {}
+
+  testConnection() {
+    this.apiService.getHello().subscribe({
+      next: (res) => this.response = res,
+      error: (err) => this.response = 'Error: ' + err.message
+    });
+  }
 }
